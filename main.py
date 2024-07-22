ae2eddd0342a0bd847c6e2807cc6e318c524e344
Lara <lara.rozman04@gmail.com>
2024-07-22 12:00:45 +0200

    Pridobitev HTML strani anime sezon v pridobi.py in prazen main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+"""Osnutek main-a"""
+
+#Kar potrebujem:
+#pridobi.py za pridobivanje HTML-jev
+# ___
+#izlusci.py, ko že imam za vsak anime HTML in iščem posamezne podatke
+#shrani.py za ustvarjanje CSV datotek
---
+++ pridobi.py
@@ -0,0 +1,20 @@
+import requests
+import os
+
+def pridobi_sezone(od, do):
+    sezone = ["winter", "spring", "summer", "fall"]
+
+    headers = {"User-agent": "Chrome/124.0.6367.202"}
+    
+    for leto in range(od, do):
+        for sezona in sezone:
+            url = f"https://myanimelist.net/anime/season/{leto}/{sezona}"
+
+            odgovor = requests.get(url, headers=headers)
+            if odgovor.status_code != 200:
+                print("Napaka", leto, sezona, odgovor.status_code)
+                return
+            
+            with open(os.path.join("Neobdelani_podatki", f"anime{leto}{sezona}.html"), "w", encoding="utf-8") as dat:
+                dat.write(odgovor.text)
+            
